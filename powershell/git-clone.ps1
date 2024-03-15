8e5338471d6a00e7d22456892b1f1c31dc2ede07
Pilkie <39397979+DazPilkie@users.noreply.github.com>
2024-03-15 13:22:12 +0000

    Latest Update

--- powershell/git-clone.ps1
+++ powershell/git-clone.ps1
@@ -1,16 +1,16 @@
 <#
 .SYNOPSIS
-This script ensures the latest version of a specified Git repository is installed and updated on the local system.
+This script ensures a fresh copy of a specified Git repository is downloaded to the local system.
 
 .DESCRIPTION
 The script performs the following actions:
-- Checks for and installs the latest version of Git if it's not already installed.
-- Installs necessary PowerShell modules (PackageManagement, AWSPowerShell, PendingReboot).
+- Checks for Git installation and stops with an error if Git is not installed.
+- Installs necessary PowerShell modules (PackageManagement, PendingReboot).
 - Determines the best available drive (prefers D:\ over C:\) to store the repository.
-- Clones the repository to the specified location if it's not already present, or pulls the latest changes if it is.
+- Deletes the repository if it already exists, then clones a fresh copy to the specified location.
 
 .PARAMETER repoUrl
-The URL of the Git repository to clone or pull. Default value is 'https://github.com/IT-Surgery/Scripts.git'.
+The URL of the Git repository to clone. Default value is 'https://github.com/IT-Surgery/Scripts.git'.
 
 .EXAMPLE
 PS> .\UpdateGitRepo.ps1
@@ -29,6 +29,20 @@ Creation Date:  03-15-2024
 # Define the repository URL
 $repoUrl = 'https://github.com/IT-Surgery/Scripts.git'
 
+# Logging function
+function Write-Log {
+    Param ([string]$Message)
+    Write-Output $Message
+    $Message | Out-File -FilePath $logFilePath -Append -Encoding UTF8
+}
+
+# Check if Git is installed
+Write-Log "Checking for Git installation"
+if (-not (Get-Command git -ErrorAction SilentlyContinue)) {
+    throw "Git must be installed to use this script."
+}
+Write-Log "Git is installed. Version: $(git --version)"
+
 # Determine the logs directory based on the availability of the D:\ drive
 $logDrive = if (Test-Path D:\) { "D:\" } else { "C:\" }
 $logPath = Join-Path -Path $logDrive -ChildPath "Logs\Git\"
@@ -43,13 +57,7 @@ $dateTime = Get-Date -Format "yyyy-MM-dd_HH-mm-ss"
 $logFile = "Git-Clone-$dateTime.log"
 $logFilePath = Join-Path -Path $logPath -ChildPath $logFile
 
-# Function to write output to both console and log file
-function Write-Log {
-    Param ([string]$Message)
-    Write-Output $Message
-    $Message | Out-File -FilePath $logFilePath -Append -Encoding UTF8
-}
-
+# Installing PowerShell Modules
 Write-Log "Installing PowerShell Modules"
 Install-PackageProvider -Name NuGet -Force
 $modules = 'PackageManagement', 'PendingReboot'
@@ -64,30 +72,24 @@ foreach ($module in $modules) {
     Import-Module $module
 }
 
-Write-Log "Install the latest version of GIT"
-if (-not (Get-Command git -ErrorAction SilentlyContinue)) {
-    $latestRelease = Invoke-RestMethod -Uri "https://api.github.com/repos/git-for-windows/git/releases/latest"
-    $downloadUrl = ($latestRelease.assets | Where-Object { $_.name -like '*64-bit.exe' }).browser_download_url
-    $installerPath = "$env:TEMP\GitInstaller.exe"
-    Invoke-WebRequest -Uri $downloadUrl -OutFile $installerPath
-    Start-Process -Wait -FilePath $installerPath -ArgumentList "/VERYSILENT"
-    Remove-Item -Path $installerPath -Force
-} else {
-    Write-Log "Git is already installed. Version: $(git --version)"
-}
-git --version | Out-File -FilePath $logFilePath -Append -Encoding UTF8
-
 # Determine the save location based on the availability of the D:\ drive
 $drive = if (Test-Path D:\) { "D:\" } else { "C:\" }
-$repoName = $repoUrl -split '/' | Select-Object -Last 1 -Skip 1
-$saveLocation = Join-Path -Path $drive -ChildPath ("Git\" + $repoName.Replace('.git', ''))
+# Extract the organization or owner and the repository name from the URL
+$urlParts = $repoUrl -split '/'
+$repoOwner = $urlParts[-2] # The second to last element is typically the owner or organization name
+$repoName = $urlParts[-1]  # The last element is the repository name
+$repoName = $repoName -replace '\.git$', '' # Remove '.git' if present
+# Construct the save location to include both the owner/organization and the repo name
+$saveLocation = Join-Path -Path $drive -ChildPath "Git\$repoOwner\$repoName"
 
 # Clone or pull the repository
 if (Test-Path $saveLocation) {
-    Write-Log "Updating the repository at $saveLocation"
-    Set-Location -Path $saveLocation
-    git pull | Out-File -FilePath $logFilePath -Append -Encoding UTF8
-} else {
-    Write-Log "Cloning the repository to $saveLocation"
-    git clone $repoUrl $saveLocation | Out-File -FilePath $logFilePath -Append -Encoding UTF8
+    Write-Log "Repository exists at $saveLocation. Deleting for a fresh clone."
+    Remove-Item -Path $saveLocation -Recurse -Force
 }
+
+Write-Log "Cloning the repository to $saveLocation"
+$gitCommand = "git clone '$repoUrl' '$saveLocation' 2>&1 | Out-File -FilePath '$logFilePath' -Append -Encoding UTF8"
+
+# Start a new PowerShell process to execute the Git command
+Start-Process powershell.exe -ArgumentList "-NoProfile", "-Command", $gitCommand -Wait
