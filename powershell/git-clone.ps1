08e813db4f5797a43b48ac3e0bb341e95ec45cc4
Pilkie <39397979+DazPilkie@users.noreply.github.com>
2024-03-15 13:36:14 +0000

    Latest Update

--- powershell/git-clone.ps1
+++ powershell/git-clone.ps1
@@ -29,6 +29,20 @@ Creation Date:  03-15-2024
 # Define the repository URL
 $repoUrl = 'https://github.com/IT-Surgery/Scripts.git'
 
+# Determine the logs directory based on the availability of the D:\ drive
+$logDrive = if (Test-Path D:\) { "D:\" } else { "C:\" }
+$logPath = Join-Path -Path $logDrive -ChildPath "Logs\Git\"
+
+# Define the log file name with current date and time
+$dateTime = Get-Date -Format "yyyy-MM-dd_HH-mm-ss"
+$logFile = "Git-Clone-$dateTime.log"
+$logFilePath = Join-Path -Path $logPath -ChildPath $logFile
+
+# Create the logs directory if it does not exist
+if (-not (Test-Path $logPath)) {
+    New-Item -ItemType Directory -Path $logPath -Force | Out-Null
+}
+
 # Logging function
 function Write-Log {
     Param ([string]$Message)
@@ -42,20 +56,6 @@ if (-not (Get-Command git -ErrorAction SilentlyContinue)) {
     throw "Git must be installed to use this script."
 }
 Write-Log "Git is installed. Version: $(git --version)"
-
-# Determine the logs directory based on the availability of the D:\ drive
-$logDrive = if (Test-Path D:\) { "D:\" } else { "C:\" }
-$logPath = Join-Path -Path $logDrive -ChildPath "Logs\Git\"
-
-# Create the logs directory if it does not exist
-if (-not (Test-Path $logPath)) {
-    New-Item -ItemType Directory -Path $logPath -Force | Out-Null
-}
-
-# Define the log file name with current date and time
-$dateTime = Get-Date -Format "yyyy-MM-dd_HH-mm-ss"
-$logFile = "Git-Clone-$dateTime.log"
-$logFilePath = Join-Path -Path $logPath -ChildPath $logFile
 
 # Installing PowerShell Modules
 Write-Log "Installing PowerShell Modules"
@@ -74,22 +74,17 @@ foreach ($module in $modules) {
 
 # Determine the save location based on the availability of the D:\ drive
 $drive = if (Test-Path D:\) { "D:\" } else { "C:\" }
-# Extract the organization or owner and the repository name from the URL
 $urlParts = $repoUrl -split '/'
 $repoOwner = $urlParts[-2] # The second to last element is typically the owner or organization name
-$repoName = $urlParts[-1]  # The last element is the repository name
-$repoName = $repoName -replace '\.git$', '' # Remove '.git' if present
-# Construct the save location to include both the owner/organization and the repo name
+$repoName = $urlParts[-1] -replace '\.git$', '' # The last element is the repository name
 $saveLocation = Join-Path -Path $drive -ChildPath "Git\$repoOwner\$repoName"
 
-# Clone or pull the repository
+# Clone or refresh the repository
 if (Test-Path $saveLocation) {
     Write-Log "Repository exists at $saveLocation. Deleting for a fresh clone."
     Remove-Item -Path $saveLocation -Recurse -Force
 }
 
 Write-Log "Cloning the repository to $saveLocation"
-$gitCommand = "git clone '$repoUrl' '$saveLocation' 2>&1 | Out-File -FilePath '$logFilePath' -Append -Encoding UTF8"
-
-# Start a new PowerShell process to execute the Git command
-Start-Process powershell.exe -ArgumentList "-NoProfile", "-Command", $gitCommand -Wait
+$gitCommand = "git clone '$repoUrl' '$saveLocation' 2>&1"
+Start-Process -FilePath "powershell.exe" -ArgumentList "-Command", $gitCommand -Wait -WindowStyle Hidden | Out-File -FilePath $logFilePath -Append -Encoding utf8
